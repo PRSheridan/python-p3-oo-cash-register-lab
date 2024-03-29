f56961c15fb4f8910c92ecbd57931c8542a1c73e
Phil <philrsheridan@gmail.com>
2024-03-29 17:00:23 -0400

    just started

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  pass
+  def __init__(self, total, discount=0):
+    self.total = total
+    self.discount = discount
+
+  @property
+  def total(self):
+    return self._total
+
+  @total.setter
+  def total(self, total):
+    pass
