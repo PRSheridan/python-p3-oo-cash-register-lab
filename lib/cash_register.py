d2511500d8b8395899ce09d4dcaa3b6bf1513707
Phil <philrsheridan@gmail.com>
2024-04-01 09:58:02 -0400

    passing all tests

--- lib/cash_register.py
+++ lib/cash_register.py
@@ -1,14 +1,29 @@
 #!/usr/bin/env python3
 
 class CashRegister:
-  def __init__(self, total, discount=0):
-    self.total = total
+  def __init__(self, discount=0):
     self.discount = discount
+    self.total = 0
+    self.items = []
+    self.previous_transactions = []
 
-  @property
-  def total(self):
-    return self._total
+  def add_item(self, item, price, quantity = 1):
+    self.total += price * quantity
+    for test in range(quantity):
+      self.items.append(item)
+    self.previous_transactions.append(
+      {"item": item, "price": price, "quantity": quantity}
+    )
 
-  @total.setter
-  def total(self, total):
-    pass
+  def apply_discount(self):
+    if self.discount > 0:
+      self.total = self.total - (self.total * (self.discount/100))
+      print(f"After the discount, the total comes to ${int(self.total)}.")
+    else:
+      print("There is no discount to apply.")
+ 
+  def void_last_transaction(self):
+    if self.previous_transactions == []:
+      print("no previous transaction")
+    else:
+      self.total -= (self.previous_transactions[-1]["price"] * self.previous_transactions[-1]["quantity"])
